092d67fd2816b15994add89fff8b5dbd2c5635de
J. Benjamin Leeds <leeds@uw.edu>
2019-02-09 17:53:28 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+
+Simple HTTP-redirect web server
+Author: J. Benjamin Leeds
+Created: February 8. 2019
+Copyright 2019 Leeds Look Listen, Inc.
+
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("hello world")
+
+	// http.ListenAndServe()
+
+}
